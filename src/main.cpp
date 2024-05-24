1139958a1358e1a0076b4a8ae04886cfa6559577
cheesebuilding <micahchen004@gmail.com>
2024-05-24 17:40:09 +0800

    Made code messy

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,93 @@
-#include "main.h"
+  #include "main.h"
 
-//ports
-#define PNEUMATICS_PORT 'A'
-#define LEFT_MOTOR_1 11
-#define LEFT_MOTOR_2 12
-#define RIGHT_MOTOR_1 1
-#define RIGHT_MOTOR_2 2
+  //ports
+  #define PNEUMATICS_PORT 'H'
+  #define LEFT_MOTOR_1 11
+  #define LEFT_MOTOR_2 12
+  #define RIGHT_MOTOR_1 1
+  #define RIGHT_MOTOR_2 2
+  #define INTAKE_MOTOR_1 8
+  #define INTAKE_MOTOR_2 9
 
+  //motor groups initialized here 
+  auto leftMotors = MotorGroup({LEFT_MOTOR_1, LEFT_MOTOR_2}); 
+  auto rightMotors = MotorGroup({RIGHT_MOTOR_1, RIGHT_MOTOR_2});
+  auto intakeMotors = MotorGroup({INTAKE_MOTOR_1, INTAKE_MOTOR_2});
 
-//qcf joystick state
-enum class JoystickState {
-  Center,
-  Up,
-  Down,
-  Left
-};
-
-//motor groups initialized here 
-auto leftMotors = MotorGroup({LEFT_MOTOR_1, LEFT_MOTOR_2}); 
-auto rightMotors = MotorGroup({RIGHT_MOTOR_1, RIGHT_MOTOR_2});
-
-//drive is initialized here
-auto drive = ChassisControllerBuilder()
-    .withMotors(leftMotors, rightMotors) 
-    .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})
-    .withOdometry()
-    .buildOdometry();
+  //drive is initialized here
+  auto drive = ChassisControllerBuilder()
+      .withMotors(leftMotors, rightMotors) 
+      .withDimensions(AbstractMotor::gearset::blue, {{4_in, 10_in}, imev5BlueTPR})
+      .withOdometry()
+      .buildOdometry();
 
 
-//controller, joystick state, and piston is initialized here
-Controller master;
-JoystickState state = JoystickState::Center;
-pros::ADIDigitalOut piston(PNEUMATICS_PORT);
+  //controller, joystick state, and piston is initialized here
+  Controller master;
+  pros::ADIDigitalOut piston(PNEUMATICS_PORT);
 
+  //odom test
+  pros::Task positionTrackingTask([](){
+      while (true) {
+          auto state = drive->getState();
 
-//pneumatics
-void pneumatics(bool& state){
-  if (master.getDigital(ControllerDigital::R1)) {
-    state = !state;
-    piston.set_value(state);
-  }
-}
-//odom test
-pros::Task positionTrackingTask([](){
-    while (true) {
-        auto state = drive->getState();
+          // Print the state to the VEX brain
+          pros::lcd::print(0, "x pos: %.2f", state.x.convert(inch));
+          pros::lcd::print(1, "y pos: %.2f", state.y.convert(inch));
+          pros::lcd::print(2, "Theta: %.2f", state.theta.convert(degree));
 
-        // Print the state to the VEX brain
-        pros::lcd::print(0, "x pos: %.2f", state.x.convert(inch));
-        pros::lcd::print(1, "y pos: %.2f", state.y.convert(inch));
-        pros::lcd::print(2, "Theta: %.2f", state.theta.convert(degree));
+          pros::delay(10);
+      }
+  });
 
-        pros::delay(10);
-    }
-});
-//main loop
 void opcontrol() {
-  //initital state
-  bool pistonState = false; 
+
+  //piston state
+  bool pistonState = false, prevPistonState = false;
+  bool intakeState = false, prevIntakeState = false;
+
 
   while (true) {
+
+    //movement values
     double left = -master.getAnalog(ControllerAnalog::leftY);
     double right = master.getAnalog(ControllerAnalog::rightY);
-    //movement
-    movement(left, right);
-    //Pneumatics
-    pneumatics(pistonState);
-    //Macros
-    //qcf();
 
-   
 
-    //Set brakes
-    if (master.getDigital(ControllerDigital::L1)) {
-        leftMotors.setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
-        rightMotors.setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
-    } else {
-        leftMotors.setBrakeMode(okapi::AbstractMotor::brakeMode::coast);
-        rightMotors.setBrakeMode(okapi::AbstractMotor::brakeMode::coast);
+    //base movement
+    drive->getModel()->tank(left, right);
+
+    //pneumatics
+    bool currentPistonState = master.getDigital(ControllerDigital::R1);
+    if (currentPistonState && !prevPistonState){
+        pistonState = !pistonState;
+            if (pistonState) piston.set_value(true);
+            else piston.set_value(false);
     }
+    prevPistonState = currentPistonState;
 
-    
+    //intake
+    bool currentAState = master.getDigital(ControllerDigital::A);
+  if (currentAState && !prevIntakeState) {
+    intakeState = !intakeState;
+    if (intakeState) {
+      intakeMotors.moveVoltage(500);
+    } else {
+      intakeMotors.moveVoltage(0);
+    }
+  }
+  prevIntakeState = currentAState;
     
 
     pros::delay(20);
   }
 }
 
-//base movement
-void movement(double left, double right){
-    drive->getModel()->tank(left, right);
-}
 
-void autonomous(){
-  Point point1 = Point{0_ft, 0_ft};
-  drive->driveToPoint(point1);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//qcf
-void qcf() {
-  double leftX = master.getAnalog(ControllerAnalog::leftX);
-  double leftY = master.getAnalog(ControllerAnalog::leftY);
-  
-  //180 degree turn
-  if (leftY > 10 && abs(leftX) < 10) {
-    state = JoystickState::Up;
-  } else if (leftY < -10 && abs(leftX) < 10) {
-    if (state == JoystickState::Up) {
-      state = JoystickState::Down;
-    }
-  } else if (leftY < 10 && leftX < -10) {
-    if (state == JoystickState::Down) {
-      state = JoystickState::Left;
-      drive->turnAngle(180_deg);
-    }
-  } else {
-    state = JoystickState::Center;
+ void autonomous(){
+    Point point1 = Point{0_ft, 0_ft};
+    drive->driveToPoint(point1);
   }
-}
+
+
+
